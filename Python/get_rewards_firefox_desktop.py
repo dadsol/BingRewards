90d07181e1b2984c024113594faee5a1e9b82e54
dadsol <76813195+dadsol@users.noreply.github.com>
2021-01-05 09:32:48 +0000

    Update get_rewards_firefox_desktop.py

--- Python/get_rewards_firefox_desktop.py
+++ Python/get_rewards_firefox_desktop.py
@@ -68,7 +68,7 @@ for num, word in enumerate(words_list):
     print('{0}. URL : {1}'.format(str(num + 1), url_base + word))
     try:
         driver.get(url_base + word)
-        driver.save_screenshot('sample_screenshot_'+num+'.png')
+        driver.save_screenshot('/tmp/sample_screenshot_'+num+'.png')
         print('\t' + driver.find_element_by_tag_name('h2').text)
     except Exception as e1:
         print(e1)
